440eff0513a64c2b5538f52ba71300251d8378e9
Oyindamola <damolaoyin005@gmail.com>
2026-02-13 14:28:13 +0100

    push

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
- const firstNumber = document.getElementsById("number-box-1");
- const secondNumber = document.getElementsById("number-box-2");
+ const firstNumber = document.getElementById("number-box-1");
+ const secondNumber = document.getElementById("number-box-2");
  const displayResult = document.getElementById("answer-box");
  const addBtn = document.querySelector(".operator-add");
  const minusBtn = document.querySelector(".operator-minus");
  const multiplyBtn = document.querySelector(".operator-multiply");
  const divideBtn = document.querySelector(".operator-divide");
+ const backBtn = document.querySelector(".operator-back")
 
  function calculate(){
     const firstnumber = parseFloat(firstNumber.value);
@@ -18,12 +19,17 @@
         firstnumber, secondnumber
     };
  }
+ function clear(){
+    firstNumber.value ="";
+    secondNumber.value ="";
+    displayResult.textContent = "";
+ }
  addBtn.addEventListener("click", () =>{
     const values = calculate();
     if (!values) return;
     displayResult.textContent = values.firstnumber + values.secondnumber;
  });
- minusBtnBtn.addEventListener("click", () =>{
+ minusBtn.addEventListener("click", () =>{
     const values = calculate();
     if (!values) return;
     displayResult.textContent = values.firstnumber - values.secondnumber;
@@ -39,4 +45,12 @@ multiplyBtn.addEventListener("click", () =>{
     displayResult.textContent = values.firstnumber / values.secondnumber;
  });
 
- console.log("Js successful")
+ backBtn.addEventListener("click" ,() => {
+    if (secondNumber.value !==""){
+        secondNumber.value =secondNumber.value.slice(0, -1);
+        displayResult.textContent ="";
+    }
+        else if (firstNumber.value!== ""){
+            firstNumber.value =firstNumber.value.slice(0, -1);;
+        }
+ });
