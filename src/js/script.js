2f2c54c95e19199918312ad1fe870d9880fd7ca6
Oyindamola <damolaoyin005@gmail.com>
2026-02-13 10:35:10 +0100

    wip

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
- const firstNumber = document.getElementsById("first-number-container");
- const secondNumber = document.getElementsById("second-number-container");
+ const firstNumber = document.getElementsById("number-box-1");
+ const secondNumber = document.getElementsById("number-box-2");
  const operators = document.querySelectorAll(".operators-container");
+ const displayResult = document.getElementById("answer-box");
+
+ function calculate(Operation){
+    const firstnumber = parseFloat(firstNumber.value);
+    const secondnumber = parseFloat(secondNumber.value);
+ }
+ 
