639029e89da2d2e958aab55101ea8c253666eeed
Oyindamola <damolaoyin005@gmail.com>
2026-02-13 10:29:23 +0100

    wip

---
+++ src/js/script.js
@@ -0,0 +1,3 @@
+ const firstNumber = document.getElementsById("first-number-container");
+ const secondNumber = document.getElementsById("second-number-container");
+ const operators = document.querySelectorAll(".operators-container");
